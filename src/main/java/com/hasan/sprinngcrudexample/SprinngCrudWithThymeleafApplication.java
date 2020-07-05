ba4c34be25c9c8127a8b32f8fedd9d3e26932ba2
HASAN <hasanuzzaman.dev@gmail.com>
2020-07-05 17:48:04 +0600

    create mvc packages

---
+++ src/main/java/com/hasan/sprinngcrudexample/SprinngCrudWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.hasan.sprinngcrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngCrudWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinngCrudWithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasan/sprinngcrudexample/SprinngCrudWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.sprinngcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinngCrudWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
